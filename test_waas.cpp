beac9a092397976fa8b574317560cc1708a5494d
pedro <hdlguy@hotmail.com>
2020-05-07 16:26:21 -0400

    failing to find preamble words

--- test_waas.cpp
+++ test_waas.cpp
@@ -37,10 +37,15 @@ int main ()
         if (1 == ((waasdat[i])>>2)&1) waassym[i*2+0] = '1'; else waassym[i*2+0] = '0';
         if (1 == ((waasdat[i])>>6)&1) waassym[i*2+1] = '1'; else waassym[i*2+1] = '0';
     }
+    std::string bits (waassym, (size_t)(filesize*2));  // ViterbiCodec::Decode wants C++ string.
+    //char waassym[] = {'0','0','0','0','1','1','1','0','1','1','1','1','1','1','1','1','0','0','1','1','0','0','0','1','1','1','0','0','0','0','0','0'};
+    //std::string bits (waassym, (size_t)32);
     //const std::string& bits = "00001110111111110011000111000000";
     //const std::string bits = "00001110111111110011000111000000";
     //const std::string bits ((size_t)(filesize*2), 'x');
-    const std::string bits (waassym, (size_t)(filesize*2));  // ViterbiCodec::Decode wants C++ string.
+    //const std::string bits (waassym, (size_t)(filesize*2));  // ViterbiCodec::Decode wants C++ string.
+    //std::string bits = "00001110111111110011000111000000";
+    //bits.insert(0, 2, '0');
 
     // set the Viterbi constraint length and polynomials.
     const int constraint = 7;
@@ -53,11 +58,24 @@ int main ()
     ViterbiCodec codec(constraint, polynomials);
     //std::cout << codec.Decode(bits) << std::endl;
     std::string result_string = codec.Decode(bits);
+    std::cout << bits << std::endl;
     std::cout << result_string << std::endl;
 
     
     // find sync pattern (if not found rerun Viterbi with other two bit framing)
+    std::string p_a("01010011"); std::string p_b("10011010"); std::string p_c("11000110");
+    std::string n_a("10101100"); std::string n_b("01100101"); std::string n_c("00111001");
 
+    std::size_t found;
+    found = result_string.find(p_a); std::cout << "p_a found at: " << found << '\n';
+    found = result_string.find(p_b); std::cout << "p_b found at: " << found << '\n';
+    found = result_string.find(p_c); std::cout << "p_c found at: " << found << '\n';
+
+    found = result_string.find(n_a); std::cout << "n_a found at: " << found << '\n';
+    found = result_string.find(n_b); std::cout << "n_b found at: " << found << '\n';
+    found = result_string.find(n_c); std::cout << "n_c found at: " << found << '\n';
+
+    std::cout << "std::string::npos = " << std::string::npos << '\n';
 
     // parse messages
 
@@ -66,7 +84,7 @@ int main ()
 
     
     free(waasdat);
-    free(waassym);
+    //free(waassym);
 }
 
 
