736fadd9e99d2ad1281ecb1ad675121e9fead0ba
pedro <hdlguy@hotmail.com>
2020-05-11 18:10:54 -0400

    trying to decode waas data

--- test_waas.cpp
+++ test_waas.cpp
@@ -17,12 +17,15 @@
 #include <string>
 #include <vector>
 
+using namespace std;
+
 int main () 
 {
 
     // read waas LLR file
     printf("opening waas.bin\n");
     FILE* fp = fopen("waas.bin", "r");
+    // get the file size.
     struct stat stat_buf;
     fstat(fileno(fp), &stat_buf);
     off_t filesize = stat_buf.st_size;
@@ -31,52 +34,68 @@ int main ()
     uint8_t* waasdat = (uint8_t*)malloc((size_t) 1*filesize);
     fread(waasdat, 1, filesize, fp);
 
-    // convert LLR data to string
+
+    // convert LLR data to string, two symbols per byte
     char* waassym = (char*)malloc((size_t) 2*filesize);
     for (int i=0; i<filesize; i++){
         if (1 == ((waasdat[i])>>2)&1) waassym[i*2+0] = '1'; else waassym[i*2+0] = '0';
         if (1 == ((waasdat[i])>>6)&1) waassym[i*2+1] = '1'; else waassym[i*2+1] = '0';
     }
-    std::string bits (waassym, (size_t)(filesize*2));  // ViterbiCodec::Decode wants C++ string.
+    string bits (waassym, (size_t)(filesize*2));  // ViterbiCodec::Decode wants C++ string.
+
     //char waassym[] = {'0','0','0','0','1','1','1','0','1','1','1','1','1','1','1','1','0','0','1','1','0','0','0','1','1','1','0','0','0','0','0','0'};
-    //std::string bits (waassym, (size_t)32);
-    //const std::string& bits = "00001110111111110011000111000000";
-    //const std::string bits = "00001110111111110011000111000000";
-    //const std::string bits ((size_t)(filesize*2), 'x');
-    //const std::string bits (waassym, (size_t)(filesize*2));  // ViterbiCodec::Decode wants C++ string.
-    //std::string bits = "00001110111111110011000111000000";
-    //bits.insert(0, 2, '0');
+    //string bits (waassym, (size_t)32);
+
+    //string bits = "00001110111111110011000111000000";
 
     // set the Viterbi constraint length and polynomials.
+    cout << "bits.length() = " << bits.length() << "\n";
     const int constraint = 7;
-    std::vector<int> polynomials;
-    polynomials.push_back(121);
-    polynomials.push_back( 91);
+    vector<int> polynomials;
+    polynomials.push_back(121);  // G1
+    polynomials.push_back( 91);  // G2
     for (int i = 0; i < polynomials.size(); i++) polynomials[i] = ReverseBits(constraint, polynomials[i]);
 
     // call the Viterbi decoder
     ViterbiCodec codec(constraint, polynomials);
-    //std::cout << codec.Decode(bits) << std::endl;
-    std::string result_string = codec.Decode(bits);
-    std::cout << bits << std::endl;
-    std::cout << result_string << std::endl;
+    string result_string = codec.Decode(bits);
+    //std::cout << bits << std::endl;
+    //std::cout << result_string << std::endl;
+
+    // preamble patterns (if not found rerun Viterbi with other two bit framing)
+    string p_a("01010011"); string p_b("10011010"); string p_c("11000110");
+
+/*
+    // faux data
+    for (int i=0; i<result_string.length()/750; i++) {
+        result_string.replace(i*750+  0, 8, p_a);
+        result_string.replace(i*750+250, 8, p_b);
+        result_string.replace(i*750+500, 8, p_c);
+    }
+*/
 
     
-    // find sync pattern (if not found rerun Viterbi with other two bit framing)
-    std::string p_a("01010011"); std::string p_b("10011010"); std::string p_c("11000110");
-    std::string n_a("10101100"); std::string n_b("01100101"); std::string n_c("00111001");
+    cout << "searching for preamble patterns in positive results\n";
+    for (int i=0; i<result_string.length()-750; i++) {
+        if ( (0==result_string.substr(i+0,8).compare(p_a)) && (0==result_string.substr(i+250,8).compare(p_b)) && (0==result_string.substr(i+500,8).compare(p_c)) ) {
+            cout << "p_a, p_b and p_c found at: " << i << '\n'; 
+        }
+    }
 
-    std::size_t found;
-    found = result_string.find(p_a); std::cout << "p_a found at: " << found << '\n';
-    found = result_string.find(p_b); std::cout << "p_b found at: " << found << '\n';
-    found = result_string.find(p_c); std::cout << "p_c found at: " << found << '\n';
+    // make copy of bits and negate it
+    string negbits (bits); 
+    for (int i=0; i<negbits.length(); i++) if (negbits[i]=='0') negbits[i]='1'; else negbits[i]='0';
+    // run viterbi decoder
+    result_string = codec.Decode(negbits);
 
-    found = result_string.find(n_a); std::cout << "n_a found at: " << found << '\n';
-    found = result_string.find(n_b); std::cout << "n_b found at: " << found << '\n';
-    found = result_string.find(n_c); std::cout << "n_c found at: " << found << '\n';
+    cout << "searching for preamble patterns in negative results\n";
+    for (int i=0; i<result_string.length()-750; i++) {
+        if ( (0==result_string.substr(i+0,8).compare(p_a)) && (0==result_string.substr(i+250,8).compare(p_b)) && (0==result_string.substr(i+500,8).compare(p_c)) ) {
+            cout << "p_a, p_b and p_c found at: " << i << '\n'; 
+        }
+    }
 
-    std::cout << "std::string::npos = " << std::string::npos << '\n';
-
+    
     // parse messages
 
 
