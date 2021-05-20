50829115d0f1b7d21a8f54b91127f7e8719cc7a6
pete <peter.dudley@precisionplanting.com>
2021-05-20 08:56:16 -0400

    OK, verilog simulation of Galeleo encoder matches the ICD appendix D tables

--- verilog/convenc.sv
+++ verilog/convenc.sv
@@ -1,17 +1,16 @@
 // a simple half rate convolutional encoder for the purpose of understanding the appendix D tables in the Galileo ICD.
-module convenc #(
-    parameter integer K = 7,                // constraint length
-    parameter integer R = 2                 // rate change
-) (
-    input   logic[K-1:0]    G[R-1:0],  // the polynomials
-    // 1st polynomial //G1 = 171 octal = 1111001 = 121 decimal
-    // 2nd polynomial //G2 = 133 octal = 1011011 =  91 decimal
+module convenc (
     input   logic           clk,
     input   logic           dv_in,
     input   logic           din,
     output  logic           dv_out,
     output  logic[1:0]      dout
 );
+
+    localparam integer K = 7;               // constraint length
+    localparam integer R = 2;               // rate change
+    localparam logic[6:0] G1 = 7'b1111001;  // 1st polynomial //G1 = 171 octal = 1111001 = 121 decimal
+    localparam logic[6:0] G2 = 7'b1011011;  // 2nd polynomial //G2 = 133 octal = 1011011 =  91 decimal
 
     logic[K-1:0] shreg = 0;
     logic dv_in_reg = 0;
@@ -21,12 +20,12 @@ module convenc #(
             shreg <= {din, shreg[K-1:1]};  // shift in on msb
         end
         
-        for (int i=0; i<R; i++) begin
-            dout[i] <= ^(G[i]&shreg);
-        end
+        dout[0] <=  ^(G1&shreg);
+        dout[1] <= ~^(G2&shreg); // note inversion
         
         dv_in_reg <= dv_in;
         dv_out <= dv_in_reg;
+        
     end
 
 endmodule
--- verilog/convenc_tb.sv
+++ verilog/convenc_tb.sv
@@ -5,7 +5,6 @@ module convenc_tb ();
     localparam integer K = 7; // constraint length
     localparam integer R = 2; // rate change
 
-    logic[K-1:0]    G[R-1:0]; // the polynomials
     logic           dv_in;
     logic           din;
     logic           dv_out;
@@ -13,10 +12,7 @@ module convenc_tb ();
 
     localparam clk_period = 10; logic clk = 0; initial forever #(clk_period/2) clk = ~clk;
 
-    convenc #(.K(K), .R(R)) uut (.*);
-
-    assign G[0] = 7'b111_1001; // 1st polynomial //G1 = 171 octal = 1111001 = 121 decimal
-    assign G[1] = 7'b101_1011; // 2nd polynomial //G2 = 133 octal = 1011011 =  91 decimal
+    convenc uut (.*);
 
     localparam Ndata = 120;
     logic[0:Ndata-1] din_array = 120'b11111111_11110000_11001100_10101010_00000000_00001111_00110011_01010101_11100011_11101100_11011111_10001010_00011100_00010011_01000000;
@@ -26,21 +22,22 @@ module convenc_tb ();
         din = 0;
         #(clk_period*10);
 
-        forever begin
-            dv_in = 1;
-            din = 0;
-            #(clk_period*10);
+//        dv_in = 1;
+//        din = 0;
+//        #(clk_period*10);
 
-            for (int i=0; i<Ndata; i++) begin
-                dv_in = 1;
-                din = din_array[i];
-                #(clk_period*1);
-            end
-
+        for (int i=0; i<Ndata; i++) begin
             dv_in = 1;
-            din = 0;
-            #(clk_period*10);
+            din = din_array[i];
+            #(clk_period*1);
         end
+
+        dv_in = 1;
+        din = 0;
+        #(clk_period*10);
+        
+        $stop;
+        
     end
     
     always_ff @(posedge clk) begin
